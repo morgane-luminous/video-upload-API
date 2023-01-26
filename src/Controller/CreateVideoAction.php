b3af054d031c95359a1ceaeb8ab40e82a096762c
Morgane Rebindaine <mrebindaine@hotmail.com>
2023-01-26 09:18:48 +0100

    Use serializer to create Video

--- src/Controller/CreateVideoAction.php
+++ src/Controller/CreateVideoAction.php
@@ -2,45 +2,26 @@
 
 namespace App\Controller;
 
-use ApiPlatform\Api\IriConverterInterface;
 use App\Entity\Video;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Attribute\AsController;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Serializer\SerializerInterface;
 
 #[AsController]
 class CreateVideoAction extends AbstractController
 {
-    public function __construct(private IriConverterInterface $iriConverter)
+    public function __construct(private SerializerInterface $serializer)
     {
     }
 
     public function __invoke(Request $request): Video
     {
+        $jsonRequest  = json_encode($request->request->all());
+        $video        = $this->serializer->deserialize($jsonRequest, Video::class, 'json');
         $uploadedFile = $request->files->get('file');
 
-        if (!$uploadedFile) {
-            throw new BadRequestHttpException('"file" is required');
-        }
-
-        $title = $request->request->get('title');
-        $description = $request->request->get('description');
-        $categories = $request->request->all()['categories'] ?? null; // https://github.com/symfony/symfony/issues/44432
-
-        $video = new Video();
-        $video->setFile($uploadedFile)
-            ->setTitle($title)
-            ->setDescription($description)
-        ;
-
-        if (is_array($categories)) {
-            $categories = array_map(function (string $category) {
-                return $this->iriConverter->getResourceFromIri($category);
-            }, $categories);
-
-            $video->setCategories($categories);
-        }
+        $video->setFile($uploadedFile);
 
         return $video;
     }
